718e229b025f3de77ca62c49d6562afd24a652f7
justdvnsh <justdvnsh2208@gmail.com>
2020-04-30 21:18:58 +0530

    added glide instance, and drawable instance from the app module class

--- Dagger2_RXJava_MVVM/app/src/main/java/com/divyansh/dagger2_rxjava_mvvm/AuthActivity.java
+++ Dagger2_RXJava_MVVM/app/src/main/java/com/divyansh/dagger2_rxjava_mvvm/AuthActivity.java
@@ -2,11 +2,18 @@ package com.divyansh.dagger2_rxjava_mvvm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
 
 import javax.inject.Inject;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import dagger.android.support.DaggerAppCompatActivity;
 
 public class AuthActivity extends DaggerAppCompatActivity {
@@ -14,13 +21,27 @@ public class AuthActivity extends DaggerAppCompatActivity {
     private final String TAG = "application";
 
     @Inject
-    boolean isAppNull;
+    RequestManager requestManager;
+
+    @Inject
+    Drawable logo;
+
+    @BindView(R.id.login_logo)
+    ImageView logoImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_auth);
 
-        Log.d(TAG, "application is null ?" + isAppNull);
+        ButterKnife.bind(this);
+
+        setLogo();
     }
+
+    private void setLogo() {
+        requestManager.load(logo).into(logoImage);
+    }
+
+
 }
--- Dagger2_RXJava_MVVM/app/src/main/java/com/divyansh/dagger2_rxjava_mvvm/di/AppModule.java
+++ Dagger2_RXJava_MVVM/app/src/main/java/com/divyansh/dagger2_rxjava_mvvm/di/AppModule.java
@@ -1,6 +1,14 @@
 package com.divyansh.dagger2_rxjava_mvvm.di;
 
 import android.app.Application;
+import android.graphics.drawable.Drawable;
+
+import androidx.core.content.ContextCompat;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.RequestManager;
+import com.bumptech.glide.request.RequestOptions;
+import com.divyansh.dagger2_rxjava_mvvm.R;
 
 import dagger.Module;
 import dagger.Provides;
@@ -10,7 +18,17 @@ import dagger.Provides;
 public class AppModule {
 
     @Provides
-    static boolean getApp(Application application) {
-        return application == null;
+    static RequestOptions providesRequestOptions(Application application) {
+        return RequestOptions.placeholderOf(R.drawable.logo).error(R.drawable.white_background);
+    }
+
+    @Provides
+    static RequestManager providesRequestManager(Application application, RequestOptions requestOptions) {
+        return Glide.with(application).setDefaultRequestOptions(requestOptions);
+    }
+
+    @Provides
+    static Drawable providesDrawable(Application application) {
+        return ContextCompat.getDrawable(application, R.drawable.logo);
     }
 }
