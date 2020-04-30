7ccb0aab3ab7cc9167e800023f505d43b05da7a5
justdvnsh <justdvnsh2208@gmail.com>
2020-04-30 17:03:38 +0530

    added dagger

---
+++ Dagger2_RXJava_MVVM/app/src/main/java/com/divyansh/dagger2_rxjava_mvvm/MainActivity.java
@@ -0,0 +1,14 @@
+package com.divyansh.dagger2_rxjava_mvvm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
